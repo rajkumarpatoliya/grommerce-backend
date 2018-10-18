780fd2ccf0688930f9967a5c921a9de481d1dfe1
w4721x <patoliyaraj@gmail.com>
2018-10-18 10:57:07 +0530

    configured jest framework

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 6 */
+
 import express from "express";
 
 const app = express();
---
+++ app.test.js
@@ -0,0 +1,5 @@
+/*jshint esversion: 6 */
+
+const app =require('./app');
+
+// test case will be written soon
